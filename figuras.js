cf773208a500fd3ce9b2ac627ffad73bc8ef67db
Alejandro Cayoja <Stronranger@gmail.com>
2021-11-02 23:16:06 -0300

    Perimetros y areas de cuadrado y triangulo

--- figuras.js
+++ figuras.js
@@ -1,1 +1,45 @@
-console.log("Hallo")
+// Código del calculo de perimetro y área de un cuadrado
+
+console.group("Cuadrados");   // console.group sirve para agrupar los mensajes en la consola pero debe tener un final determinado por console.groupEnd()
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden: " + ladoCuadrado + "cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es: " + perimetroCuadrado + "cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es: " + areaCuadrado + "cm2");
+console.groupEnd();
+
+// Código del calculo de perimetro y área de un triángulo
+
+console.group("Triángulo"); 
+const ladoTriángulo1 = 6;
+const ladoTriángulo2 = 6;
+const baseTriángulo = 4;
+
+console.log("Los lados del triángulo miden: " 
++ ladoTriángulo1 
++ "cm, " 
++ ladoTriángulo2 
++ "cm, " 
++ baseTriángulo 
++ "cm"
+);
+
+const alturaTriángulo = 5.5;
+console.log("La altura del triángulo es de: " + alturaTriángulo + "cm");
+
+const perimetroTriángulo = ladoTriángulo1 + ladoTriángulo2 + baseTriángulo;
+console.log("El perimetro del triángulo es: " + perimetroTriángulo + "cm");
+
+const areaTriángulo = ( baseTriángulo * alturaTriángulo ) / 2;
+console.log("El area del triángulo es: " + areaTriángulo + "cm2");
+
+console.groupEnd();
+
+// Código del calculo de perimetro y área de una círculo
+
+console.group("círculo"); 
+
+console.groupEnd();
